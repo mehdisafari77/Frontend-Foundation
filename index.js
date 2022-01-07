00d2979bbeb92b85b94ce95cc4377b2561b95725
Mehdi Safari <sa.mehdisafari@gmail.com>
2022-01-07 10:03:32 -0800

    add alerts, and edit contact form

--- index.js
+++ index.js
@@ -1,23 +1,17 @@
 
 
-
-
-console.log("hello world");
-
+// Compliment Message on Mouseover
 let catImage = document.getElementById("#cat-image");
-function handleSubmit(evt) {
-	evt.preventDefault();
-	
-	alert('Your form has been submitted!, Thank you');
-}
-
 function complimentMessage(e){
 	e.preventDefault();
     alert('Hi')
 }
-
 catImage.addEventListener('mouseover', complimentMessage)
 
-let form = document.querySelector('form#contact');
-
+// Send form
+function handleSubmit(evt) {
+	evt.preventDefault();
+	alert('Your message has been sent!, Thank you');
+}
+let form = document.getElementById('send');
 form.addEventListener('submit', handleSubmit);
---
+++ profile.js
@@ -0,0 +1,4 @@
+let favClr = document.getElementById('color')
+let favPlc = document.getElementById('place')
+let favRtl = document.getElementById('ritual')
+
