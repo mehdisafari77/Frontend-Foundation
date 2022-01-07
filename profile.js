12afc6c12361ecb1d7be04676e139a18dda7393d
Mehdi Safari <sa.mehdisafari@gmail.com>
2022-01-07 10:07:05 -0800

    finish profile alerts

--- profile.js
+++ profile.js
@@ -2,3 +2,12 @@ let favClr = document.getElementById('color')
 let favPlc = document.getElementById('place')
 let favRtl = document.getElementById('ritual')
 
+function favClrAlert() {
+    alert('My Favorite Color is Blue')
+}
+function favPlcAlert() {
+    alert('My Favorite Place is London')
+}
+function favRtlAlert() {
+    alert('My Favorite Ritual is Morning Coffee')
+}
