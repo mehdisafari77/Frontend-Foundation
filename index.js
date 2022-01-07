f3192caef1d8683d4a7164a974e55dd54fe5c01e
Mehdi Safari <sa.mehdisafari@gmail.com>
2022-01-07 09:07:31 -0800

    change styling of navbars and edits in profile page

---
+++ index.js
@@ -0,0 +1,21 @@
+console.log("hello world");
+
+let catImage = document.getElementById("#cat-image");
+
+
+function handleSubmit(evt) {
+	evt.preventDefault();
+	
+	alert('Your form has been submitted!, Thank you');
+}
+
+function complimentMessage(e){
+	e.preventDefault();
+    alert('Hi')
+}
+
+catImage.addEventListener('mouseover', complimentMessage)
+
+let form = document.querySelector('form#contact');
+
+form.addEventListener('submit', handleSubmit);
