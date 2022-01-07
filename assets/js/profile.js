d785edf0305847b4218879b6d870467f9ed5ca2a
Mehdi Safari <sa.mehdisafari@gmail.com>
2022-01-07 11:14:06 -0800

    finish profile page fully

--- assets/js/profile.js
+++ assets/js/profile.js
@@ -1,11 +1,3 @@
- // typing text animation script
-var typed = new Typed(".typing-2", {
-    strings: ["IOS Developer", "Web-Developer", "Student"],
-    typeSpeed: 100,
-    backSpeed: 60,
-    loop: true
-});
-
 let favClr = document.getElementById('color')
 let favPlc = document.getElementById('place')
 let favRtl = document.getElementById('ritual')
