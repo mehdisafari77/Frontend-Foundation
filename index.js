f21b5d442bf33f2bbf4af3f8e03ff01b9e82f674
Mehdi Safari <sa.mehdisafari@gmail.com>
2022-01-07 09:30:59 -0800

    edit top home section

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
+ // typing text animation script
+ var typed = new Typed(".typing", {
+    strings: ["IOS Developer", "Web-Developer", "Student"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
+var typed = new Typed(".typing-2", {
+    strings: ["IOS Developer", "Web-Developer", "Student"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
+
+
+
 console.log("hello world");
 
 let catImage = document.getElementById("#cat-image");
-
-
 function handleSubmit(evt) {
 	evt.preventDefault();
 	
