b940453244361031e474991523577acd85d4fe1e
Mehdi Safari <sa.mehdisafari@gmail.com>
2022-01-07 10:52:06 -0800

    style edits

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
-
-
-
 // Compliment Message on Mouseover
 let catImage = document.getElementById("#cat-image");
-function complimentMessage(e){
-	e.preventDefault();
-    alert('Hi')
+function complimentMessage(x){
+    alert('Hi! My name is Alex The Cat')
 }
+
 catImage.addEventListener('mouseover', complimentMessage)
 
 // Send form
+let form = document.getElementById('send');
 function sendMessage() {
 	alert('Your message has been sent!, Thank you');
 }
-let form = document.getElementById('send');
