14a4510434e774fb04bbf41de635a0492adbd075
Mehdi Safari <sa.mehdisafari@gmail.com>
2022-01-07 09:45:28 -0800

    navbar edits

--- index.js
+++ index.js
@@ -1,16 +1,4 @@
- // typing text animation script
- var typed = new Typed(".typing", {
-    strings: ["IOS Developer", "Web-Developer", "Student"],
-    typeSpeed: 100,
-    backSpeed: 60,
-    loop: true
-});
-var typed = new Typed(".typing-2", {
-    strings: ["IOS Developer", "Web-Developer", "Student"],
-    typeSpeed: 100,
-    backSpeed: 60,
-    loop: true
-});
+
 
 
 
