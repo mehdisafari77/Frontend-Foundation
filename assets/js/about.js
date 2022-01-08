6c75c6b993691f81c246b954165adc9aeefa9860
Mehdi Safari <sa.mehdisafari@gmail.com>
2022-01-08 15:17:32 -0800

    add location boxes plus styling

--- assets/js/about.js
+++ assets/js/about.js
@@ -2,3 +2,8 @@ let locationBtn = document.getElementById('locations')
 function showLocations() {
     alert('Catopia Currently Operates In All 50 States In The US')
 }
+
+let la = document.getElementsByClassName('secondary-box')
+function randomLocationSF() {
+    
+}
