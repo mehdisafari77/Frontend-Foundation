408ecdedf0a0a880e236d111847fe8bc1eceda4c
Mehdi Safari <sa.mehdisafari@gmail.com>
2022-01-08 15:28:06 -0800

    add 1st random selector

--- assets/js/about.js
+++ assets/js/about.js
@@ -3,7 +3,11 @@ function showLocations() {
     alert('Catopia Currently Operates In All 50 States In The US')
 }
 
-let la = document.getElementsByClassName('secondary-box')
+let la = document.getElementsById('secondary-box')
 function randomLocationSF() {
-    
+    var sfRestaurants = ['Cocotte', "Morton's Steakhouse", 'Chipotle', "The Cheesecake Factory"]; 
+    const random = Math.floor(Math.random() * sfRestaurants.length);
+    console.log(random, sfRestaurants[random]);
+    randomResult =  sfRestaurants[random]
+    alert(`Your random restaurant selection result: ${randomResult}`)
 }
